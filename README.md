363a8a7254b70bad293eee9e507efd1ef3fcb720
sacha.van.weeren <sacha.van.weeren@gmail.com>
2020-12-15 17:14:14 +0100

    updated readme and introduced module click in the code

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Demo project to manage AWS EC2 instance snapshots
 
+## Remark 
+
+Code does not run in a notebook nor in spyder. 
+You really need to use the command line
+
 ## About
 
 This project is a demo, and uses boto3 to manage 
@@ -15,4 +20,4 @@ Shotty uses the configuration file created by the AWS cli. e.g.
 
 ## Running
 
-python shotty/shotty.py
+`python shotty/shotty.py`
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,12 +1,24 @@
 # The python package to connect to AWS
 import boto3
+#import sys
+import click
 
-if __name__ == '__main__':
-    # Create session with your credentials
-    session = boto3.Session(profile_name='iam_user')
+# Create session with your credentials
+session = boto3.Session(profile_name='iam_user')
 
-    # connect to an AWS rescources
-    ec2 = session.resource('ec2', region_name='us-east-1')
+# connect to an AWS rescources
+ec2 = session.resource('ec2', region_name='us-east-1')
 
+#@click.command()
+def list_instances():
+    "List EC2 instances"
     for i in ec2.instances.all():
-        print(i)
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+        
+if __name__ == '__main__':
+    list_instances()
