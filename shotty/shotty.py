ab2c52fdbac9b0b060cec945d218a9b61a337f4e
sacha.van.weeren <sacha.van.weeren@gmail.com>
2020-12-16 08:38:51 +0100

    Created groups, filters

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -3,22 +3,43 @@ import boto3
 #import sys
 import click
 
+
 # Create session with your credentials
 session = boto3.Session(profile_name='iam_user')
 
 # connect to an AWS rescources
 ec2 = session.resource('ec2', region_name='us-east-1')
 
-#@click.command()
-def list_instances():
+@click.group()
+def instances():
+    """Commands for instances"""
+
+    
+@instances.command('list')
+@click.option('--project', default=None,
+             help="Only instances for project (tag Project:<name)")
+def list_instances(project):
     "List EC2 instances"
-    for i in ec2.instances.all():
+    instances = []
+    
+    if project:
+        filters = [{'Name':'tag:Name','Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+        
+    for i in instances:
+        #print(i.tags)
+        tags = {element['Key']:element['Value'] for element in i.tags or []}
+        #print(tags)
         print(', '.join((
-            i.id,
-            i.instance_type,
-            i.placement['AvailabilityZone'],
-            i.state['Name'],
-            i.public_dns_name)))
+                i.id,
+                i.instance_type,
+                i.placement['AvailabilityZone'],
+                i.state['Name'],
+                i.public_dns_name,
+                tags.get('Project','<no projects>'))))
+        print()
         
 if __name__ == '__main__':
-    list_instances()
+    instances()
